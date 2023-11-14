dcf293edd6bbe59ec49b2eabc66ef3a68a995cb7
AxelR <axel.ronnberg.25@gmail.com>
2023-11-14 13:54:28 +0100

    test

--- body.py
+++ body.py
@@ -7,14 +7,14 @@ class Body():
         self.mass = mass
         self.radius = radius
         self.position = [position[0], position[1]]    # (x, y)
-        self.velocity = velocity    # (x, y)
+        self.velocity = [velocity[0], velocity[1]]    # (x, y)
         self.collor = collor        # (r, g, b)
 
 
     def draw(self, screen):
         #we take the position and add half of the screen size to it so that the center of the screen is 0,0
-        coordsX = (self.position[0]/1000000) + screen.get_size()[0]/2
-        coordsY = (self.position[1]/1000000) + screen.get_size()[1]/2
+        coordsX = (self.position[0]/1000000000) + screen.get_size()[0]/2
+        coordsY = (self.position[1]/1000000000) + screen.get_size()[1]/2
 
         #Prints the circle
         pygame.draw.circle(screen, self.collor, (coordsX, coordsY), self.radius)
@@ -24,7 +24,16 @@ class Body():
         self.position[0] += self.velocity[0]
         self.position[1] += self.velocity[1]
 
-    def calc_velocity(self, other):
-        #calculate the velocity of the body
+    def calc_velocity(self, other, time_step=86400*30):
+        G = 6.6743e-11  # gravitational constant
+        dx = other.position[0] - self.position[0]
+        dy = other.position[1] - self.position[1]
+        dist = (dx**2 + dy**2)**0.5
+        force = G * (self.mass * other.mass) / (dist**2)
+        ax = force / self.mass * (dx / dist)
+        ay = force / self.mass * (dy / dist)
 
-        pass
+        self.velocity[0] += ax * time_step
+        self.velocity[1] += ay * time_step
+        other.velocity[0] -= ax * self.mass / other.mass * time_step
+        other.velocity[1] -= ay * self.mass / other.mass * time_step
--- main.py
+++ main.py
@@ -6,6 +6,8 @@ import sys
 
 WIDTH, HEIGHT = 640, 480
 
+#one frame is 1 second
+FPS = 60
 
 def main():
 
@@ -21,16 +23,18 @@ def main():
     sun_color = (255, 255, 0)
     sun = Body(sun_pos, sun_v, sun_mass, sun_radius, sun_color)
     #Earth position. mean distance to sun = 1.496×10^8 km.
-    #23481 x earths radius.
-    earth_pos = (1.496*(10**8), 0)
+    #23481 x earths radius. and convert to m
+    earth_pos = (1.496*(10**8)*1000, 0)
     #Earth velocity = 107,200 km/h orbiting around the sun
-    earth_v = (0, 107200)
+    # made to m/s = 29,7777778 m/s
+    earth_v = (0, 107200 * 0.277777778)
     #Earth mass = 5.972168×10^24 kg
     earth_mass = 5.972168*(10**24)
     #Earth mean radius = 6371 km
     earth_radius = 10
     earth_color = (0, 0, 255)
     earth = Body(earth_pos, earth_v, earth_mass, earth_radius, earth_color)
+    #earth = Body(earth_pos, (0, 0), earth_mass, earth_radius, earth_color)
     #stores the bodys in a list
     bodys = []
     bodys.append(sun)
@@ -54,7 +58,6 @@ def main():
         for i, b1 in enumerate(bodys):
             for b2 in bodys[i+1:]:
                 b1.calc_velocity(b2)
-                b2.calc_velocity(b1)
 
         for b in bodys:
             b.update_pos()
@@ -63,7 +66,9 @@ def main():
         for b in bodys:
             b.draw(screen)
 
-        sleeptime = 1/60 - (time.time() - last_frame)
+        sleeptime = 1/FPS - (time.time() - last_frame)
+        if sleeptime < 0:
+            sleeptime = 0
         time.sleep(sleeptime)
         last_frame = time.time()
         pygame.display.flip()
